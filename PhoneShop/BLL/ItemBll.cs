0e121cf89574b93d59b7326afb668716612a596e
dudubaobei1848 <dudubaobei1848@qq.com>
2020-05-02 20:20:11 +0800

    添加bll；设置用户bll

---
+++ PhoneShop/BLL/ItemBll.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL
+{
+    /// <summary>
+    /// 商品Bll
+    /// </summary>
+    public class ItemBll
+    {
+    }
+}
