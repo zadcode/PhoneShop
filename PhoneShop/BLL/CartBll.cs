aa318710503323a6d6241bdd8c7257483d463ace
dudubaobei1848 <dudubaobei1848@qq.com>
2020-05-02 22:40:20 +0800

    设置CartBll，FavorBll

--- PhoneShop/BLL/CartBll.cs
+++ PhoneShop/BLL/CartBll.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,58 @@ namespace BLL
     /// </summary>
     public class CartBll
     {
+        /// <summary>
+        /// 购物车查询
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <returns>数据表</returns>
+        public DataTable GetFavor(int id)
+        {
+            //  排序为店家正序
+            return null;
+        }
+
+        /// <summary>
+        /// 添加购物车
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="itemid">商品id</param>
+        /// <param name="color">颜色</param>
+        /// <param name="model">型号</param>
+        /// <param name="price">价格</param>
+        /// <param name="count">数量</param>
+        /// <returns>true：成功；false：失败</returns>
+        public bool AddFavor(int id, int itemid, string color, string model, int price, int count)
+        {
+            //  店家id根据商品id获取，时间为当前时间
+            return false;
+        }
+
+        /// <summary>
+        /// 删除购物车
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="itemid">商品id</param>
+        /// <returns>true：成功；false：失败</returns>
+        public bool Delete(int id, int itemid)
+        {
+            return false;
+        }
+
+        /// <summary>
+        /// 更改购物车
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="itemid">商品id</param>
+        /// <param name="color">颜色</param>
+        /// <param name="model">型号</param>
+        /// <param name="price">价格</param>
+        /// <param name="count">数量</param>
+        /// <returns>true：成功；false：失败</returns>
+        public bool UpdateFavor(int id, int itemid, string color, string model, int price, int count)
+        {
+            //  店家id根据商品id获取，时间为当前时间
+            return false;
+        }
     }
 }
--- PhoneShop/BLL/FavorBll.cs
+++ PhoneShop/BLL/FavorBll.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,41 @@ namespace BLL
     /// <summary>
     /// 收藏Bll
     /// </summary>
-    public class FavorBll
+    public class FavorBll   
     {
+        /// <summary>
+        /// 收藏查询
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <returns>数据表</returns>
+        public DataTable GetFavor(int id)
+        {
+            //  排序为时间倒序
+            return null;
+        }
+
+        /// <summary>
+        /// 添加收藏
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="itemid">商品id</param>
+        /// <returns>true：成功；false：失败</returns>
+        public bool AddFavor(int id, int itemid)
+        {
+            //  店家id根据商品id获取，时间为当前时间
+            return false ;
+        }
+
+        /// <summary>
+        /// 删除收藏
+        /// </summary>
+        /// <param name="id">用户id</param>
+        /// <param name="itemid">商品id</param>
+        /// <returns>true：成功；false：失败</returns>
+        public bool Delete(int id,int itemid)
+        {
+            return false;
+        }
     }
+
 }
